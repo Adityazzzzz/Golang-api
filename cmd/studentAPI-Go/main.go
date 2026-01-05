25d42611332f0a389841a7a4f04d08093cd85f84
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-01-05 15:04:19 +0530

    API

--- cmd/studentAPI-Go/main.go
+++ cmd/studentAPI-Go/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Adityazzzzz/studentAPI-Go/intenal/config"
+)
 
 func main() {
 	fmt.Println("Goland_api")
+/*
+TODO:
+	load config
+	db setup
+	routes
+	server
+*/
+
+//config
+	cfg := config.MustLoad()
+
+//db
+
+//routes
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(res http.ResponseWriter, req *http.Request){
+		res.Write([]byte("welcome to api"))
+	})
+
+//server
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	err:=server.ListenAndServe()
+	if err!=nil {
+		log.Fatal("Failed to start server")
+	}
+
+	fmt.Println("server started")
 }
